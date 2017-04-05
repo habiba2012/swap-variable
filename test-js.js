b5dc13a6a2c838c2d67bace78d1172667e100169
habiba akhter <habibaakhter2012@gmail.com>
2017-04-05 11:08:02 +0200

    task2.html

---
+++ test-js.js
@@ -0,0 +1,18 @@
+var x= 5;
+var y = 10;
+
+function swap(x, y) {
+  return [y, x]
+}
+
+//[x, y] = swap(x, y)
+
+if (x!=y){
+	x=Y;
+	y=x;
+	document.write("value of x = " + x +"/" + "value of y = " + y);
+}
+
+else
+
+document.write("x= "+ x + "y = " + y);
